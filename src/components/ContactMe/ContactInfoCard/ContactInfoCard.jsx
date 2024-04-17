f3db56eb2cf5e10cc9f245dcd320c1e3cee7e0af
hicham <h.echarifchefchaouni@ecole-ipssi.net>
2024-04-17 12:07:12 +0200

    git link

--- src/components/ContactMe/ContactInfoCard/ContactInfoCard.jsx
+++ src/components/ContactMe/ContactInfoCard/ContactInfoCard.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "./ContactInfoCard.css";
 
-const ContactInfoCard = ({ iconUrl, text }) => {
+const ContactInfoCard = ({ iconUrl, text, isLink }) => {
   return (
+    (isLink ) ?
     <div className="contact-details-card">
-    <a href={text} target="_blank" rel="noreferrer">
+      <div className="contact-icon">
+        <img src={iconUrl} alt={text} />
+      </div>
+      <a href={text}>{text}</a>
+    </div>
+      
+     : 
+      <div className="contact-details-card">
       <div className="contact-icon">
         <img src={iconUrl} alt={text} />
       </div>
       <p>{text}</p>
-    </a>
     </div>
+    
   );
 };
 
--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -4,20 +4,28 @@ import ContactInfoCard from "./ContactInfoCard/ContactInfoCard";
 import ContactForm from "./ContactForm/ContactForm";
 
 const ContactMe = () => {
-    return (
-        <section className="contact-container">
-            <h5>Contact Me</h5>
-            <div className="contact-content">
-                <div style={{flex: 1}}>
-                    <ContactInfoCard iconUrl="/images/mail.png" text="hicham.echarifchefchaouni@gmail.com" />
-                    <ContactInfoCard iconUrl="/images/github.png" text="https://github.com/hichamrox" />
-                </div>
-                <div style={{flex: 1}}>
-                    <ContactForm />
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="contact-container">
+      <h5>Contact Me</h5>
+      <div className="contact-content">
+        <div style={{ flex: 1 }}>
+          <ContactInfoCard
+            iconUrl="/images/mail.png"
+            text="hicham.echarifchefchaouni@gmail.com"
+            isLink={false}
+          />
+          <ContactInfoCard
+            iconUrl="/images/github.png"
+            text="https://github.com/hichamrox"
+            isLink={true}
+          />
+        </div>
+        <div style={{ flex: 1 }}>
+          <ContactForm />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default ContactMe
+export default ContactMe;
