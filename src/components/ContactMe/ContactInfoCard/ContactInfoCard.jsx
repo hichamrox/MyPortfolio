84b1fe2f89989f7ef2c634845de3c9878555a80f
hicham <h.echarifchefchaouni@ecole-ipssi.net>
2024-04-17 12:03:37 +0200

    git link

--- src/components/ContactMe/ContactInfoCard/ContactInfoCard.jsx
+++ src/components/ContactMe/ContactInfoCard/ContactInfoCard.jsx
@@ -4,10 +4,12 @@ import "./ContactInfoCard.css";
 const ContactInfoCard = ({ iconUrl, text }) => {
   return (
     <div className="contact-details-card">
+    <a href={text} target="_blank" rel="noreferrer">
       <div className="contact-icon">
         <img src={iconUrl} alt={text} />
       </div>
       <p>{text}</p>
+    </a>
     </div>
   );
 };
